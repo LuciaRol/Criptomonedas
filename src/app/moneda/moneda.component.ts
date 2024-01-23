781fb569b600dfdc9e726e9cfce1af013e7652f4
LuciaRol <luciarol87@gmail.com>
2024-01-23 22:20:54 +0100

    comandos ejecutados

---
+++ src/app/moneda/moneda.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-moneda',
+  standalone: true,
+  imports: [],
+  templateUrl: './moneda.component.html',
+  styleUrl: './moneda.component.css'
+})
+export class MonedaComponent {
+
+}
